a1940612cceb722aaaabd29e4c6cccc53b035227
xclusivePc <xmt@abv.bg>
2015-06-07 19:24:02 +0300

    update

---
+++ NthDigit/NthDigit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class NthDigit
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        int n = int.Parse(Console.ReadLine());
+
+        double nDigit;
+
+        nDigit = (number / Math.Pow(10, n - 1)) % 10;
+        if ((int)nDigit == 0)
+        {
+            Console.WriteLine('-');
+        }
+        else
+        {
+            Console.WriteLine((int)nDigit);
+        }
+    }
+}
