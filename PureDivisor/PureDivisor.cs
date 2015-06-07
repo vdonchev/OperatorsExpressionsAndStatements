2658fde369839721aed9836e5e900bfe5507c21e
xclusivePc <xmt@abv.bg>
2015-06-07 20:16:28 +0300

    update

---
+++ PureDivisor/PureDivisor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class PureDivisor
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        bool result;
+
+        if (n % 9 == 0 || n % 11 == 0 || n % 13 == 0)
+        {
+            result = true;
+        }
+        else
+        {
+            result = false;
+        }
+
+        Console.WriteLine(result);
+    }
+}
