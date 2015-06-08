8eae7c49bbece286c0bf38c93cf7a81dd204990a
xclusivePc <xmt@abv.bg>
2015-06-08 18:46:32 +0300

    update

---
+++ PthBit/PthBit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class PthBit
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int p = int.Parse(Console.ReadLine());
+        int bitAtPositionP;
+
+        bitAtPositionP = (n >> p) & 1;
+        Console.WriteLine(bitAtPositionP);
+    }
+}
---
+++ TriBitSwitch/TriBitSwitch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        long n = long.Parse(Console.ReadLine());
+        int p = int.Parse(Console.ReadLine());
+
+        long mask = (7 << p);
+        long result = (n ^ mask);
+
+        Console.WriteLine(result);
+    }
+}
