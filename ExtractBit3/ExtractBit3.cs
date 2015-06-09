a74d7d0cfc6dd1a568a666b71cb5bdbe1be56128
xclusivePc <xmt@abv.bg>
2015-06-09 12:55:56 +0300

    update

---
+++ ExtractBit3/ExtractBit3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class ExtractBit3
+{
+    static void Main()
+    {
+        int num = int.Parse(Console.ReadLine());
+        Console.WriteLine(Convert.ToString(num, 2).PadLeft(16, '0'));
+
+        for (int i = 0; i < 3; i++)
+        {
+            int mask = 
+        }
+    }
+}
